bdc97b7d9d50bcd4c4beb65bc623e4f9e6faa1d7
Lawrence <lawrence2000april@gmail.com>
2021-09-28 12:56:45 +0530

    Initial commit

---
+++ newsapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+# Create your views here.
+def index(request):
+    newsapi = NewsApiClient(api_key = '7fbe8a43036d44a198e94b58be8d85a1')
+    top = newsapi.get_top_headlines(sources='the-hindu',language='en')
+    l = top['articles']
+    desc=[]
+    news=[]
+    img=[]
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news,desc,img)
+    context = {'mylist':mylist}
+    return render(request,'newsapp/news.html',context)
