83e4f9c9a141c0a2a6964eac4d1adc38c73a3dbc
Lawrence <lawrence2000april@gmail.com>
2021-10-02 11:43:19 +0530

    search bar

--- newsapp/views.py
+++ newsapp/views.py
@@ -3,6 +3,8 @@ from newsapi import NewsApiClient
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        print(request.POST)
     newsapi = NewsApiClient(api_key = '7fbe8a43036d44a198e94b58be8d85a1')
     top = newsapi.get_top_headlines(sources='the-hindu',language='en')
     l = top['articles']
